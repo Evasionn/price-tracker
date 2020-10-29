c33d2ef40eb516c7bacf95793ccb5084f3a11b0c
Evasionn <asalyazilim@gmail.com>
2020-10-29 21:54:17 +0300

    code refactor for writing less code on scrapper

--- src/price_tracker/scraper.py
+++ src/price_tracker/scraper.py
@@ -9,42 +9,46 @@ from price_tracker.user_agent import get_random_user_agent
 urllib3.disable_warnings()
 
 
+def request_sender(url, ssl_verification_bypass=False):
+    if ssl_verification_bypass:
+        page = requests.get(url, headers=get_random_user_agent(), verify=False)
+    else:
+        page = requests.get(url, headers=get_random_user_agent())
+    return BeautifulSoup(page.content, 'html.parser')
+
+
 class Scraper:
     def __init__(self, mailer):
         self.mailer = mailer
 
+    def mail_decider(self, url, product_name, price, warn_price):
+        if price < warn_price:
+            self.mailer.send_mail(url, product_name, price)
+            return True
+        return False
+
     def check_hepsiburada_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(id='product-name').get_text().strip()
         price = float(
             re.sub(r'\D', '', soup.find(id='offering-price').get_text().split(',')[0])
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_gittigidiyor_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(id='sp-title').get_text().strip()
         price = float(
             re.sub(r'\D', '', soup.find(class_='lastPrice').get_text().replace('\n', '').split(',')[0])
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_trendyol_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(class_='pr-nm').get_text().strip()
         price = soup.find(class_='prc-dsc')
@@ -54,15 +58,11 @@ class Scraper:
         price = float(
             re.sub(r'\D', '', price.get_text().split(',')[0])
         )
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_amazon_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(id='productTitle').get_text().strip()
 
@@ -74,15 +74,10 @@ class Scraper:
             re.sub(r'\D', '', price.get_text().split(',')[0])
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_vatan_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(class_='product-list__product-name').get_text().strip()
 
@@ -90,14 +85,10 @@ class Scraper:
             re.sub(r'\D', '', soup.find(class_='product-list__price').get_text().strip())
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_teknosa_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent(), verify=False)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url, True)
 
         product_name = soup.find(class_='product-title').get_text().strip()
 
@@ -105,14 +96,10 @@ class Scraper:
             re.findall(r"cd_product_price: '(.*)'", soup.prettify())[0]
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_n11_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent(), verify=False)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url, True)
 
         product_name = soup.find(class_='proName').get_text().strip()
 
@@ -120,15 +107,10 @@ class Scraper:
             re.findall(r'"lowPrice": "(.*)"', soup.prettify())[0]
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_ciceksepeti_net_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(class_='o-productDetail__title').get_text().strip()
 
@@ -136,15 +118,10 @@ class Scraper:
             re.sub(r'\D', '', soup.find(class_='m-productPrice__value').get_text().strip())
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_ciceksepeti_com_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find(class_='product__info__title').get_text().strip()
 
@@ -152,15 +129,10 @@ class Scraper:
             re.sub(r'\D', '', soup.find(class_='product__info__new-price__integer').get_text().strip())
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
 
     def check_mediamarkt_product(self, url: str, warn_price: float) -> bool:
-        page = requests.get(url, headers=get_random_user_agent())
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = request_sender(url)
 
         product_name = soup.find('title').get_text().strip()
 
@@ -168,7 +140,4 @@ class Scraper:
             re.sub(r'\D', '', soup.select_one('div.price.big').get_text().strip())
         )
 
-        if price < warn_price:
-            self.mailer.send_mail(url, product_name, price)
-            return True
-        return False
+        return self.mail_decider(url, product_name, price, warn_price)
